003d94eff4212759f2d15d5984c77aa71d6b2e90
LeoRoi <kuzne4eg@gmail.com>
2018-11-17 23:08:27 +0100

    jersey dependency

---
+++ songsServlet/src/main/java/de/htw/ai/kbe/songs_servlet/MyServlet.java
@@ -0,0 +1,69 @@
+package de.htw.ai.kbe.songs_servlet;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+
+public class MyServlet extends HttpServlet {
+    //    public static void main(String[] args) {
+//        System.out.println("coming soon");
+//    }
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        // Set the response message's MIME type
+        response.setContentType("text/html;charset=UTF-8");
+        // Allocate a output writer to write the response message into the network socket
+        PrintWriter out = response.getWriter();
+
+        // Write the response message, in an HTML page
+        try {
+            out.println("<!DOCTYPE html>");
+            out.println("<html><head>");
+            out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
+            out.println("<title>Hello, World</title></head>");
+            out.println("<body>");
+            out.println("<h1>Hello, world!</h1>");  // says Hello
+            // Echo client's request information
+            out.println("<p>Request URI: " + request.getRequestURI() + "</p>");
+            out.println("<p>Protocol: " + request.getProtocol() + "</p>");
+            out.println("<p>PathInfo: " + request.getPathInfo() + "</p>");
+            out.println("<p>Remote Address: " + request.getRemoteAddr() + "</p>");
+            // Generate a random number upon each request
+            out.println("<p>A Random Number: <strong>" + Math.random() + "</strong></p>");
+            out.println("</body>");
+            out.println("</html>");
+        } finally {
+            out.close();  // Always close the output writer
+        }
+    }
+}
