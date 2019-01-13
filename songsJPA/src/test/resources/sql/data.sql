81ea5aa983b49ecf50f4eb191d4de6b8e5866e3d
LeoRoi <kuzne4eg@gmail.com>
2019-01-13 09:29:13 +0100

    connection pool properties; SQL Error: -5501, SQLState: 42501 DAO test

--- songsJPA/src/test/resources/sql/data.sql
+++ songsJPA/src/test/resources/sql/data.sql
@@ -2,13 +2,13 @@
 -- insert into Address(street, city, zip, country, contact_id) values ('FirstStr 1', 'Berlin', '12345', 'Ger', 1);
 -- insert into Address(street, city, zip, country, contact_id) values ('SecondStr 2', 'Celle', '54321', 'Ger', 1);
 
-insert into songs values(1, 'Can''t Stop the Feeling', 'Justin Timberlake', 'Trolls', 2016);
-insert into songs values(2, 'Mom', 'Meghan Trainor, Kelli Trainor', 'Thank You', 2016);
-insert into songs values(3, 'Team', 'Iggy Azalea', 'null', 2016);
-insert into songs values(4, 'Ghostbusters (I''m not a fraid)', 'Fall Out Boy, Missy Elliott', 'Ghostbusters', 2016);
-insert into songs values(5, 'Bad Things', 'Camila Cabello, Machine Gun Kelly', 'Bloom', 2017);
-insert into songs values(6, 'I Took a Pill in Ibiza', 'Mike Posner', 'At Night, Alone.', 2016);
-insert into songs values(7, 'i hate u, i love u', 'Gnash', 'Top Hits 2017', 2017);
-insert into songs values(8, 'No', 'Meghan Trainor', 'Thank You', 2016);
-insert into songs values(9, 'Private Show', 'Britney Spears', 'Glory', 2016);
-insert into songs values(10, '7 Years', 'Lukas Graham', 'Lukas Graham (Blue Album)', 2015);
+insert into songs(id, title, artist, album, released) values(1, 'Can''t Stop the Feeling', 'Justin Timberlake', 'Trolls', 2016);
+insert into songs(id, title, artist, album, released) values(2, 'Mom', 'Meghan Trainor, Kelli Trainor', 'Thank You', 2016);
+insert into songs(id, title, artist, album, released) values(3, 'Team', 'Iggy Azalea', 'null', 2016);
+insert into songs(id, title, artist, album, released) values(4, 'Ghostbusters (I''m not a fraid)', 'Fall Out Boy, Missy Elliott', 'Ghostbusters', 2016);
+insert into songs(id, title, artist, album, released) values(5, 'Bad Things', 'Camila Cabello, Machine Gun Kelly', 'Bloom', 2017);
+insert into songs(id, title, artist, album, released) values(6, 'I Took a Pill in Ibiza', 'Mike Posner', 'At Night, Alone.', 2016);
+insert into songs(id, title, artist, album, released) values(7, 'i hate u, i love u', 'Gnash', 'Top Hits 2017', 2017);
+insert into songs(id, title, artist, album, released) values(8, 'No', 'Meghan Trainor', 'Thank You', 2016);
+insert into songs(id, title, artist, album, released) values(9, 'Private Show', 'Britney Spears', 'Glory', 2016);
+insert into songs(id, title, artist, album, released) values(10, '7 Years', 'Lukas Graham', 'Lukas Graham (Blue Album)', 2015);
